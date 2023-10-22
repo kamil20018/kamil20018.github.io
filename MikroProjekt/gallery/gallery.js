d103ee6e38d948d1e03686ff617227d27ab8f3ee
Kamil Bizoń <matbiz01@gmail.com>
2023-10-22 15:41:15 +0200

    init

---
+++ MikroProjekt/gallery/gallery.js
@@ -0,0 +1,18 @@
+imgPath = "../images/gallery/stock"
+currImage = 1
+imageCount = 5
+
+document.getElementById("left").addEventListener("click", leftClick);
+document.getElementById("right").addEventListener("click", rightClick);
+image = document.getElementById("image")
+
+function leftClick(){
+    currImage = currImage - 1;
+    if(currImage < 0) {currImage = imageCount - 1};
+    image.src = imgPath + currImage + ".jpg";
+}
+
+function rightClick(){
+    currImage = (currImage + 1) % 5;
+    image.src = imgPath + currImage + ".jpg"
+}
